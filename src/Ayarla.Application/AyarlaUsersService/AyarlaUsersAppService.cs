e93325892a41d13a91c51c136171122b3d3cca8d
Rakuzan-Developer <190758+Rakuzan-Developer@users.noreply.github.com>
2020-12-31 10:47:41 +0300

    AyarlaUsersServices

---
+++ src/Ayarla.Application/AyarlaUsersService/AyarlaUsersAppService.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Ayarla.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayarla.AyarlaUsersService
+{
+    public class AyarlaUsersAppService : AsyncCrudAppService<AyarlaUsers, AyarlaUsersDto, Guid, AyarlaFavoriteDto, AyarlaCommentDto, AyarlaUsersDto>, IAyarlaUsersAppService
+    {
+        public AyarlaUsersAppService(IRepository<AyarlaUsers, Guid> repository) : base(repository)
+        {
+        }
+    }
+}
---
+++ src/Ayarla.Application/AyarlaUsersService/Dto/AyarlaFavoriteDto.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Ayarla.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayarla.AyarlaUsersService
+{
+    [AutoMapFrom(typeof(AyarlaFavorite))]
+    public class AyarlaFavoriteDto : FullAuditedEntityDto<Guid>
+    {
+        public int accountId { get; set; }
+        public int userId { get; set; }
+
+    }
+}
