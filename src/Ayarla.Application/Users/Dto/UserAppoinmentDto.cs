8c40dc30b29f4eb5d9ad7be938d3040050faed63
Rakuzan-Developer <190758+Rakuzan-Developer@users.noreply.github.com>
2021-01-31 14:31:45 +0300

    UserAppservice içerisinde düzeltmeler yapıldı

---
+++ src/Ayarla.Application/Users/Dto/UserAppoinmentDto.cs
@@ -0,0 +1,25 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Ayarla.Authorization.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayarla.Users.Dto
+{
+    [AutoMapFrom(typeof(Appoinment))]
+    public class UserAppoinmentDto : FullAuditedEntityDto<Guid>
+    {
+        public int ServiceId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Day { get; set; }
+
+        public string Time { get; set; }
+    }
+}
