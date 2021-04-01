047fbfc668c78dc3205eb232b53ab3e6e0bb8e10
Ahmed KAYA <ahmedakky@gmail.com>
2021-04-01 20:36:05 +0300

    hatalar mevcut

---
+++ src/Ayarla.Application/Users/Dto/InsertFavoriteDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Ayarla.Authorization.Users;
+
+namespace Ayarla.Users.Dto
+{
+    [AutoMapTo(typeof(Favorite))]
+    public class InsertFavoriteDto : FullAuditedEntityDto<Guid>
+    {
+        public Guid AccountId { get; set; }
+    }
+}
